c42cea2de3b7070640801976bdf6c2c10436d176
SharifAhmed <sharifahmed2070@gmail.com>
2024-10-08 17:47:39 +0500

    Revert "LogIn,Signup & Landing Page"

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import store from './store'; 
-import router from './router';
+import './assets/main.css'
 
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.min.css';
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 
-const app = createApp(App);
-app.use(store); 
-app.use(router);
-app.mount('#app');
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,34 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Hello from '../views/Main_Landing.vue';
-import User_login from '../components/User_login.vue'; 
-import User_signup from '../components/User_signup.vue';
-//import Bussiness_Landing from '../views/Bussiness_Landing.vue';
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Hello 
-  },
-  {
-    path: '/User_login',
-    name: 'User_login',
-    component: User_login 
-  },
-  {
-    path: '/User_signup',
-    name: 'User_signup',
-    component: User_signup 
-  },
-  // {
-  //   path: '/Bussiness_Landing',
-  //   name: 'Bussiness_Landing',
-  //   component: Bussiness_Landing 
-  // },
-];
+import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
 
-export default router;
+  ]
+})
+
+export default router
