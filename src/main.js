211d8e596f426a436470a08e35e1e18e9d529e78
Umar-Rajpoot <ranamuhammadumar111@gmail.com>
2024-10-08 17:54:42 +0500

    umar-checkout

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import store from './store'; 
+import router from './router';
+import './assets/fonts/style.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+// 
 
-import App from './App.vue'
-import router from './router'
-
-const app = createApp(App)
-
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+import 'bootstrap/dist/css/bootstrap.min.css';
+// 
+const app = createApp(App);
+app.use(store); 
+app.use(router);
+app.mount('#app');
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,24 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import NavbarHeader from '../views/NavbarHeader.vue'
+import CheckOut from '../components/CheckOut.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'header',
+        component: NavbarHeader,
+        children:[
+          {
+            path: '/',
+            name: 'checkout',
+            component: CheckOut,
+          }
+        ]
+    }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  history: createWebHistory(),
+  routes,
+});
 
-  ]
-})
-
-export default router
+export default router;
