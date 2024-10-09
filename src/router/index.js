3792b2fde1e9d9cb522cc09a7e4fd140affbf076
ZahaRehman <zaharehman788@gmail.com>
2024-10-09 15:36:31 +0500

    Modal boxes & Revenue Reports

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,16 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router';
+import Hello from '../views/hello.vue'
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: Hello // Lazy-loaded
+      }
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
+  history: createWebHistory(),
+  routes,
+});
 
-  ]
-})
-
-export default router
+export default router;
