a925dff51ea8db7bffbaeb7eb47a19d7161b1e85
hurara12 <hurara.waseem@gmail.com>
2024-10-09 17:17:07 +0500

    choices and menu

--- src/components/MenuAndChoice.vue
+++ src/components/MenuAndChoice.vue
@@ -1,5 +1,17 @@
 <template>
     <div class="container-fluid">
+        <div>
+            <div
+                class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+                <h1 class="h2">Delivery Menu</h1>
+            </div>
+
+            <input v-model="newCategory" type="text" placeholder="Enter category name" class="form-control mb-2" />
+            <div v-if="message" :class="['alert', messageType]">
+                {{ message }}
+            </div>
+            <button class="btn btn-primary" @click="addCategory">Add Category</button>
+        </div>
         <div class="row">
             <!-- Sidebar -->
             <nav id="sidebar" class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse">
@@ -18,16 +30,13 @@
                             </a>
                             <ul class="nav flex-column ms-3">
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Breakfast</a>
+                                    <a class="nav-link" href="#" @click.prevent="OpenProductCategory">Breakfast</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Sandwiches</a>
+                                    <a class="nav-link" href="#" @click.prevent="OpenProductCategory">Sandwiches</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Pasta</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">Combo Set</a>
+                                    <a class="nav-link" href="#" @click.prevent="OpenProductCategory">Pasta</a>
                                 </li>
                             </ul>
                         </li>
@@ -37,11 +46,7 @@
 
             <!-- Main content -->
             <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                <div
-                    class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
-                    <h1 class="h2">Delivery Menu</h1>
-                    <button class="btn btn-primary">Add New</button>
-                </div>
+
 
                 <!-- Example Category Section -->
                 <div class="category-section mb-4">
@@ -52,6 +57,8 @@
                     <div class="menu-items">
                         <div class="menu-item d-flex justify-content-between align-items-center border-bottom py-2">
                             <MenuViewChoice v-if="isChoiceGroupsVisible" />
+                            <MenuView v-if="isProductCategoryVisibile" />
+                            <!-- <MenuViewChoice v-if="isMenuVisible" /> -->
                         </div>
                     </div>
                 </div>
@@ -63,13 +70,31 @@
 <script setup>
 import { ref } from 'vue';
 import MenuViewChoice from './MenuViewChoice.vue';
-
+import MenuView from './MenuView.vue';
+const newCategory = ref('');
+const message = ref('');
+const messageType = ref('alert-success'); // Default message type
 // State to track visibility of the ChoiceGroups component
 const isChoiceGroupsVisible = ref(false);
+const isProductCategoryVisibile = ref(false);
 
+const addCategory = () => {
+    if (newCategory.value.trim() !== '') {
+        // Logic to add the category can be added here
+        message.value = `Category added: ${newCategory.value}`;
+        messageType.value = 'alert-success'; // Success message type
+        newCategory.value = ''; // Clear the input field after adding
+    } else {
+        message.value = 'Please enter a category name.';
+        messageType.value = 'alert-danger'; // Error message type
+    }
+};
 // Method to toggle the visibility
 const showChoices = () => {
     isChoiceGroupsVisible.value = !isChoiceGroupsVisible.value; // Toggle visibility
+};
+const OpenProductCategory = () => {
+    isProductCategoryVisibile.value = true;
 };
 </script>
 
