4561efce232a3418f238dcd3a540106f2fe10377
Muhammad Ali <muhammaddali908@gmail.com>
2024-10-11 10:01:53 +0500

    All code files converted to <script setup>

--- src/views/Login_Signup/Bussiness_Landing.vue
+++ src/views/Login_Signup/Bussiness_Landing.vue
@@ -15,27 +15,32 @@
             <div class="form-group mb-3 input-icon">
               <label for="first_name">First Name</label>
               <i class="fas fa-user text-primary"></i>
-              <input type="text" v-model="form.first_name" class="form-control" id="first_name" placeholder="Enter your first name" required />
+              <input type="text" v-model="form.first_name" class="form-control" id="first_name"
+                placeholder="Enter your first name" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="last_name">Last Name</label>
               <i class="fas fa-user text-primary"></i>
-              <input type="text" v-model="form.last_name" class="form-control" id="last_name" placeholder="Enter your last name" required />
+              <input type="text" v-model="form.last_name" class="form-control" id="last_name"
+                placeholder="Enter your last name" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="email">Email</label>
               <i class="fas fa-envelope text-danger"></i>
-              <input type="email" v-model="form.email" class="form-control" id="email" placeholder="Enter your email address" required />
+              <input type="email" v-model="form.email" class="form-control" id="email"
+                placeholder="Enter your email address" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="password">Password</label>
               <i class="fas fa-lock text-warning"></i>
-              <input type="password" v-model="form.password" class="form-control" id="password" placeholder="Create a password" required />
+              <input type="password" v-model="form.password" class="form-control" id="password"
+                placeholder="Create a password" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="password_confirmation">Confirm Password</label>
               <i class="fas fa-lock text-warning"></i>
-              <input type="password" v-model="form.password_confirmation" class="form-control" id="password_confirmation" placeholder="Confirm your password" required />
+              <input type="password" v-model="form.password_confirmation" class="form-control"
+                id="password_confirmation" placeholder="Confirm your password" required />
             </div>
           </div>
 
@@ -45,17 +50,20 @@
             <div class="form-group mb-3 input-icon">
               <label for="address">Address</label>
               <i class="fas fa-map-marker-alt text-danger"></i>
-              <input type="text" v-model="form.address" class="form-control" id="address" placeholder="Enter the branch address" required />
+              <input type="text" v-model="form.address" class="form-control" id="address"
+                placeholder="Enter the branch address" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="postal_code">Postal Code</label>
               <i class="fas fa-mail-bulk text-primary"></i>
-              <input type="text" v-model="form.postal_code" class="form-control" id="postal_code" placeholder="Enter postal code" required />
+              <input type="text" v-model="form.postal_code" class="form-control" id="postal_code"
+                placeholder="Enter postal code" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="city">City</label>
               <i class="fas fa-city text-success"></i>
-              <input type="text" v-model="form.city" class="form-control" id="city" placeholder="Enter city name" required />
+              <input type="text" v-model="form.city" class="form-control" id="city" placeholder="Enter city name"
+                required />
             </div>
           </div>
 
@@ -110,22 +118,26 @@
             <div class="form-group mb-3 input-icon">
               <label for="cnic">CNIC</label>
               <i class="fas fa-id-card text-info"></i>
-              <input type="text" v-model="form.cnic" class="form-control" id="cnic" placeholder="Enter your CNIC number" required />
+              <input type="text" v-model="form.cnic" class="form-control" id="cnic" placeholder="Enter your CNIC number"
+                required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="bank_name">Bank Name</label>
               <i class="fas fa-university text-success"></i>
-              <input type="text" v-model="form.bank_name" class="form-control" id="bank_name" placeholder="Enter your bank name" required />
+              <input type="text" v-model="form.bank_name" class="form-control" id="bank_name"
+                placeholder="Enter your bank name" required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="iban">IBAN</label>
               <i class="fas fa-money-check text-warning"></i>
-              <input type="text" v-model="form.iban" class="form-control" id="iban" placeholder="Enter your IBAN" required />
+              <input type="text" v-model="form.iban" class="form-control" id="iban" placeholder="Enter your IBAN"
+                required />
             </div>
             <div class="form-group mb-3 input-icon">
               <label for="account_owner_title">Account Owner Title</label>
               <i class="fas fa-user-tie text-primary"></i>
-              <input type="text" v-model="form.account_owner_title" class="form-control" id="account_owner_title" placeholder="Enter account owner title" required />
+              <input type="text" v-model="form.account_owner_title" class="form-control" id="account_owner_title"
+                placeholder="Enter account owner title" required />
             </div>
           </div>
 
@@ -146,107 +158,97 @@
     </div>
   </div>
 </template>
-<script>
+<script setup>
 import {
   ref
 } from "vue";
 
-export default {
-  setup() {
-      const currentPage = ref(1);
-      const totalPages = 4;
-      const form = ref({
-          first_name: "",
-          last_name: "",
-          email: "",
-          password: "",
-          password_confirmation: "",
-          no_of_branches: "",
-          address: "",
-          postal_code: "",
-          city: "",
-          opening_time: "",
-          closing_time: "",
-          cuisine: "",
-          logo: null,
-          business_type: "",
-          cnic: "",
-          bank_name: "",
-          iban: "",
-          account_owner_title: "",
-      });
 
-      // Validate only fields relevant to the current page
-      const nextPage = () => {
-          if (validatePage()) {
-              currentPage.value++;
-          } else {
-              alert("Please fill all the required fields.");
-          }
-      };
+const currentPage = ref(1);
+const totalPages = 4;
+const form = ref({
+  first_name: "",
+  last_name: "",
+  email: "",
+  password: "",
+  password_confirmation: "",
+  no_of_branches: "",
+  address: "",
+  postal_code: "",
+  city: "",
+  opening_time: "",
+  closing_time: "",
+  cuisine: "",
+  logo: null,
+  business_type: "",
+  cnic: "",
+  bank_name: "",
+  iban: "",
+  account_owner_title: "",
+});
 
-      const prevPage = () => {
-          currentPage.value--;
-      };
-
-      const validatePage = () => {
-          switch (currentPage.value) {
-              case 1:
-                  return (
-                      form.value.first_name &&
-                      form.value.last_name &&
-                      form.value.email &&
-                      form.value.password &&
-                      form.value.password_confirmation
-                  );
-              case 2:
-                  return (
-                      form.value.no_of_branches &&
-                      form.value.address &&
-                      form.value.postal_code &&
-                      form.value.city
-                  );
-              case 3:
-                  return (
-                      form.value.opening_time &&
-                      form.value.closing_time &&
-                      form.value.cuisine &&
-                      form.value.logo &&
-                      form.value.business_type
-                  );
-              case 4:
-                  return (
-                      form.value.cnic &&
-                      form.value.bank_name &&
-                      form.value.iban &&
-                      form.value.account_owner_title
-                  );
-              default:
-                  return false;
-          }
-      };
-
-      const handleFileUpload = (event) => {
-          const file = event.target.files[0];
-          form.value.logo = file;
-      };
-
-      const submitForm = () => {
-          console.log("Form data:", form.value);
-          alert("Form Submitted!");
-      };
-
-      return {
-          currentPage,
-          totalPages,
-          form,
-          nextPage,
-          prevPage,
-          handleFileUpload,
-          submitForm,
-      };
-  },
+// Validate only fields relevant to the current page
+const nextPage = () => {
+  if (validatePage()) {
+    currentPage.value++;
+  } else {
+    alert("Please fill all the required fields.");
+  }
 };
+
+const prevPage = () => {
+  currentPage.value--;
+};
+
+const validatePage = () => {
+  switch (currentPage.value) {
+    case 1:
+      return (
+        form.value.first_name &&
+        form.value.last_name &&
+        form.value.email &&
+        form.value.password &&
+        form.value.password_confirmation
+      );
+    case 2:
+      return (
+        form.value.no_of_branches &&
+        form.value.address &&
+        form.value.postal_code &&
+        form.value.city
+      );
+    case 3:
+      return (
+        form.value.opening_time &&
+        form.value.closing_time &&
+        form.value.cuisine &&
+        form.value.logo &&
+        form.value.business_type
+      );
+    case 4:
+      return (
+        form.value.cnic &&
+        form.value.bank_name &&
+        form.value.iban &&
+        form.value.account_owner_title
+      );
+    default:
+      return false;
+  }
+};
+
+const handleFileUpload = (event) => {
+  const file = event.target.files[0];
+  form.value.logo = file;
+};
+
+const submitForm = () => {
+  console.log("Form data:", form.value);
+  alert("Form Submitted!");
+};
+
+     
+
 </script>
 
 <style scoped>
@@ -269,7 +271,7 @@ export default {
 .form-container {
 
   width: 50%;
-  background-color:#eff5f3;
+  background-color: #eff5f3;
 }
 
 .form-card {
@@ -315,71 +317,84 @@ h2 {
   background-color: #00a166;
   color: white;
 }
+
 .input-icon {
-position: relative;
-margin-bottom: 1.5rem; /* Spacing between input fields */
+  position: relative;
+  margin-bottom: 1.5rem;
+  /* Spacing between input fields */
 }
 
 .input-icon i {
-position: absolute;
-left: 15px; /* Space from the left edge */
-top: 70%;
-transform: translateY(-50%); /* Center the icon vertically */
-font-size: 1.2rem; /* Icon size */
-color: #007bff; /* Icon color */
+  position: absolute;
+  left: 15px;
+  /* Space from the left edge */
+  top: 70%;
+  transform: translateY(-50%);
+  /* Center the icon vertically */
+  font-size: 1.2rem;
+  /* Icon size */
+  color: #007bff;
+  /* Icon color */
 }
 
 .input-icon input,
 .input-icon select {
-padding-left: 45px; /* Padding to make space for the icon */
-border-radius: 0.25rem; /* Rounded corners */
-border: 1px solid #ced4da; /* Border color */
-transition: border-color 0.3s; /* Smooth border color transition */
+  padding-left: 45px;
+  /* Padding to make space for the icon */
+  border-radius: 0.25rem;
+  /* Rounded corners */
+  border: 1px solid #ced4da;
+  /* Border color */
+  transition: border-color 0.3s;
+  /* Smooth border color transition */
 }
 
 .input-icon input:focus,
 .input-icon select:focus {
-border-color: #80bdff; /* Border color on focus */
-outline: none; /* Remove default outline */
-box-shadow: 0 0 0.2rem rgba(0,123,255,.25); /* Focus shadow effect */
+  border-color: #80bdff;
+  /* Border color on focus */
+  outline: none;
+  /* Remove default outline */
+  box-shadow: 0 0 0.2rem rgba(0, 123, 255, .25);
+  /* Focus shadow effect */
 }
 
 label {
-margin-bottom: 0.5rem; /* Space between label and input */
-font-weight: 600; /* Bold labels */
-color: #333; /* Label color */
+  margin-bottom: 0.5rem;
+  /* Space between label and input */
+  font-weight: 600;
+  /* Bold labels */
+  color: #333;
+  /* Label color */
 }
 
 /* Responsive Design */
 @media (max-width: 768px) {
   .background-image {
-      display: none;
+    display: none;
   }
 
   .form-container {
-      width: 100%;
+    width: 100%;
   }
 
   .form-card {
-      margin: 2rem auto;
-      max-width: 90%;
+    margin: 2rem auto;
+    max-width: 90%;
   }
 }
 
 @media (max-width: 576px) {
   .form-card {
-      padding: 1rem;
+    padding: 1rem;
   }
 
   h2 {
-      font-size: 1.5rem;
+    font-size: 1.5rem;
   }
 
   .btn {
-      font-size: 0.8rem;
+    font-size: 0.8rem;
   }
 }
 </style>
-
-
-
