057967ab55174a9a37fc3827328d300ed3842f4c
hurara12 <hurara.waseem@gmail.com>
2024-10-15 12:09:14 +0500

    file added

--- src/Services/menu/menuProductServices.js
+++ src/Services/menu/menuProductServices.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const token = `Bearer ${'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwOi8vMTkyLjE2OC4xNS4yMDU6ODAwMC9hcGkvbG9naW4iLCJpYXQiOjE3Mjg5MDg4NzAsImV4cCI6MTcyODkxMjQ3MCwibmJmIjoxNzI4OTA4ODcwLCJqdGkiOiI5aWFBN29ZSFdKUEF6M0RMIiwic3ViIjoiMjciLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3Iiwicm9sZSI6bnVsbH0.FDvCPiiYh5nQrKPpuSDAEdeTO1JKkA19guHWlagz2T4'}`;
 const headers = { Authorization: token };
 const baseURL = import.meta.env.VITE_API_BASE_URL;
-
+//ee
 /**
  * Create FormData for product data
  * @param {Object} productData - product details including name, price, image, etc.
