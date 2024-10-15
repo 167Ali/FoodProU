e5f66e0425ad83f7c7538f11f3e8cfe023dc1df7
SAYYAM SHAHID <sayyamshahid115@gmail.com>
2024-10-15 11:33:06 +0500

    updated

--- src/Services/customer/CustomerProfile.js
+++ src/Services/customer/CustomerProfile.js
@@ -1,7 +1,7 @@
 // services/profileService.js
 import axios from 'axios';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
+const API_URL = 'http://192.168.15.90:8000/api/customers/';
 
 export const updateProfile = async (profileData) => {
     try {
