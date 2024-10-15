e5e166f5ad623f93851f8991c649bb96d58bbcbd
SAYYAM SHAHID <sayyamshahid115@gmail.com>
2024-10-15 11:56:44 +0500

    UPDATED

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,8 @@
-<<<<<<< HEAD
-<script setup>
-</script>
-
-<template>
-<router-view></router-view>
-=======
-
 <template>
 
     <RouterView></RouterView>
 
 
->>>>>>> 695c954b4ad78a5ab5abceb3dd50e7ceffee2d6e
 </template>
 
 
