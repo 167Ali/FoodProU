47c1b00e1e027e14b81caee187f38adf4e223b5b
Ali Hussain <122424617+167Ali@users.noreply.github.com>
2024-10-16 16:29:21 +0500

    Updated route of hello

--- src/views/hello.vue
+++ src/views/hello.vue
@@ -1,21 +1,37 @@
 <!-- HomeView.vue -->
+
 <!-- HomeView.vue -->
+
 <template>
-  <div>
+
+    <div>
+    
     <SeeReviews />
-    <Moreinfo />
+    
+    <!-- <Moreinfo /> -->
+    
     <CreateDeals />
+    
     <AdminFinanceDashboard />
+    
     </div>
-  </template>
-
-  <script setup>
-  import SeeReviews from '@/components/Customer/SeeReviews.vue';
-  import Moreinfo from '@/components/Customer/Moreinfo.vue';
-  import CreateDeals from '@/components/Admin/CreateDeals.vue'
-  import AdminFinanceDashboard from '@/components/Admin/AdminFinanceDashboard.vue';
-  </script>
-  
-  <style scoped>
-  </style>
-
+    
+    </template>
+    
+    
+    
+    <script setup>
+    
+    import SeeReviews from '@/components/Customer/SeeReviews.vue';
+    
+   // import Moreinfo from '@/components/Customer/Moreinfo.vue';
+    
+    import CreateDeals from '@/components/Admin/CreateDeals.vue'
+    
+    import AdminFinanceDashboard from '@/components/Admin/AdminFinanceDashboard.vue';
+    
+    </script>
+    
+    <style scoped>
+    
+    </style>    
