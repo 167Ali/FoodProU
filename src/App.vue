30e4c9222e6f22d1b0da3b4017b510f4d16f76ec
sharifahmed07 <sharifahmed2070@gmail.com>
2024-10-16 13:12:01 +0500

    Updated code

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
-<script setup>
-import hello from './views/hello.vue';
-</script>
-
 <template>
-<hello/>    
+    <RouterView></RouterView>
 </template>
 
-
 <style scoped>
-
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
 </style>
 
+<script setup>
+import { RouterView } from 'vue-router';
+</script>
 
-<style scoped></style>
+
+
+
