0ea2d5af9ea7bb978524496a5281f92da9b22fdf
Muhammad Ali <muhammaddali908@gmail.com>
2024-10-21 12:27:12 +0500

    commit before pull

--- src/Services/customer/CustomerProfile.js
+++ src/Services/customer/CustomerProfile.js
@@ -12,7 +12,7 @@ const apiClient = axios.create({
 export const api = {
   async getProfile() {
     const response = await apiClient.get('/api/customers/profile');
-    return response;  // Return the full response to access `response.data`
+    return response;  
   },
 
   async updateProfile(profileData) {
