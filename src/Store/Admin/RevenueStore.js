3a12f21eb7b4058e54c687531ba76f967061664e
SAYYAM SHAHID <sayyamshahid115@gmail.com>
2024-10-24 17:03:17 +0500

    updated code

---
+++ src/Store/Admin/RevenueStore.js
@@ -0,0 +1,50 @@
+// store/Admin/RevenueStore.js
+
+import { fetchRevenueReports } from '@/Services/Admin/RevenueService';
+
+const state = () => ({
+  revenueDetails: null,
+  orderVolumeDetails: null,
+});
+
+const getters = {
+  getRevenueDetails: (state) => state.revenueDetails,
+  getOrderVolumeDetails: (state) => state.orderVolumeDetails,
+};
+
+const actions = {
+  async fetchRevenueReports({ commit }) {
+    try {
+      const response = await fetchRevenueReports();
+      console.log('Response received in store:', response); // Log response
+
+      if (response.status === 200) {
+        commit('setRevenueDetails', response.data.data.revenue_details);
+        commit('setOrderVolumeDetails', response.data.data.order_volume_details);
+      } else {
+        console.error('Failed to fetch revenue reports', response);
+      }
+    } catch (error) {
+      console.error('Error fetching revenue reports:', error);
+    }
+  },
+};
+
+const mutations = {
+  setRevenueDetails(state, revenueDetails) {
+    state.revenueDetails = revenueDetails;
+    console.log('Revenue Details set in state:', state.revenueDetails); // Log state
+  },
+  setOrderVolumeDetails(state, orderVolumeDetails) {
+    state.orderVolumeDetails = orderVolumeDetails;
+    console.log('Order Volume Details set in state:', state.orderVolumeDetails); // Log state
+  },
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  actions,
+  mutations,
+};
--- src/Store/index.js
+++ src/Store/index.js
@@ -12,7 +12,7 @@ import order from './Modules/order'; // import the order module
 import Prevorder from './Modules/prevOrderDetail';
 import Resturantownerreviews from './Modules/resturantOwnerStore';
 import Reviews from './Modules/customerReviews';
-
+import RevenueStore from './Admin/RevenueStore';
 import restOwnerProfileStore from './RestaurantOwner/restOwnerProfileStore';
 
 import AddReviews from './Modules/addReviews';
@@ -28,6 +28,7 @@ import ProductAddToCart from '@/Components/Customer/ProductAddToCart.vue';
 const store = createStore({
   modules: {
     menuCategory,
+    RevenueStore,
     menuProduct,
     menuChoice,
     order, // register the order module
