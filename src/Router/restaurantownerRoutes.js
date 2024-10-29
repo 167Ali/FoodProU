46ad597a2db9f603873646297e16bde594486e3a
hurara12 <hurara.waseem@gmail.com>
2024-10-29 13:35:07 +0500

    a

--- src/Router/restaurantownerRoutes.js
+++ src/Router/restaurantownerRoutes.js
@@ -3,6 +3,7 @@ import RestaurantOwner_Dashboard from '@/Views/RestauranOnwer/RestaurantOwnerDas
 import ResturantOwner from '@/Components/RestaurantOwner/ResturantOwner.vue';
 import RestOwnerProfile from '@/Components/RestaurantOwner/RestOwnerProfile.vue';
 import ResturantReviews from '@/Components/RestaurantOwner/ResturantReviews.vue';
+import OrderReq from '@/Components/RestaurantOwner/RestaurantDashboard/OrderRequest.vue';
 import ModalView from '@/Components/RestaurantOwner/RestOwnReciept.vue';
 import OwnerMenu from '@/Views/Menu/OwnerMenu.vue';
 import ViewAllOrdersRes from '@/Views/RestauranOnwer/OrdersRestaurant.vue';
@@ -77,6 +78,15 @@ export default [
     path: '/setNewPassword',
     name: 'SetNewPassword',
     component: SetNewPassword,
+    meta: {
+      requiresAuth: true,
+      role: 'Restaurant Owner',
+    },
+  },
+  {
+    path: '/orderreq',
+    name: 'OrderReq',
+    component: OrderReq,
     meta: {
       requiresAuth: true,
       role: 'Restaurant Owner',
